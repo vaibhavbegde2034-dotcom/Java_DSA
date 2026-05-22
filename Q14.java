f9190964ece991dfd72ea56729ea635275139542
vaibhavbegde2034 <vaibhavbegde2034@gmail.com>
2026-05-22 14:29:14 +0530

    22-05-2026

---
+++ Q14.java
@@ -0,0 +1,52 @@
+/*
+practice Q14
+find the bigger than target and smallest element in the array
+
+arr={1,5,7,9,13,14,15,18,22,40}
+target =20
+output = 22
+
+*/
+
+public class Q14 {
+
+    public static void main(String[] args)
+    {
+        int[] arr={1,5,7,9,13,14,15,18,22,40};
+        int  target =20;
+        System.out.println(biggerthan_smalest(arr,target));
+        
+    }
+    static int biggerthan_smalest(int[] arr, int target)
+    {
+        int s=0;
+        int e= arr.length-1;
+        
+        while(s<=e)
+        {
+            int m= (s+e)/2;
+            if(arr[m]== target)
+            {
+                return arr[m];
+            }
+            else if(arr[m]< target)
+            {
+                s=m+1;
+            }
+            else
+            {
+                e=m-1;
+            }
+        }
+        return arr[s];
+
+      
+    }
+    
+}
+/*
+PS D:\JAVA_DSA> javac Q14.java
+PS D:\JAVA_DSA> java Q14      
+22
+
+*/
