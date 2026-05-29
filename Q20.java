7979990873c567184857fb36ac2dfc77c9cd6a79
vaibhavbegde2034 <vaibhavbegde2034@gmail.com>
2026-05-29 15:04:22 +0530

    29-05-2026

---
+++ Q20.java
@@ -0,0 +1,30 @@
+public class Q20 {
+    public static void main(String[] args)
+    {
+        int [] nums= {2, 4, 8, 12, 10, 5, 1};
+        System.out.println(findPeakElement(nums));
+    }
+
+    static int findPeakElement(int[] nums)
+    {
+        int s=0;
+        int e= nums.length-1;
+
+        while(s<e)
+        {
+            int m= (s+e)/2;
+            if(nums[m]<nums[m+1])
+            {
+                s=m+1;
+            }
+            else 
+            {
+                e=m-1;
+
+            }
+            
+        }
+        return nums[s];
+    }
+    
+}
